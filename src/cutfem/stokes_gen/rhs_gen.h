94c3887a73318840eb9975377ea898b1677896c6
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2020-11-14 17:12:08 +0100

    Use StokesCylinder as starting point.

---
+++ src/cutfem/stokes_gen/rhs_gen.h
@@ -0,0 +1,63 @@
+#ifndef MICROBUBBLE_RHS_GEN_H
+#define MICROBUBBLE_RHS_GEN_H
+
+#include <deal.II/base/function.h>
+#include <deal.II/base/point.h>
+#include <deal.II/base/tensor_function.h>
+
+using namespace dealii;
+
+namespace GeneralizedStokes {
+
+    struct Error {
+        double mesh_size = 0;
+        double l2_error_u = 0;
+        double h1_error_u = 0;
+        double h1_semi_u = 0;
+        double l2_error_p = 0;
+        double h1_error_p = 0;
+        double h1_semi_p = 0;
+    };
+
+    template<int dim>
+    class RightHandSide : public TensorFunction<1, dim> {
+    public:
+        Tensor<1, dim>
+        value(const Point<dim> &p) const override;
+    };
+
+
+    template<int dim>
+    class BoundaryValues : public TensorFunction<1, dim> {
+    public:
+        Tensor<1, dim>
+        value(const Point<dim> &p) const override;
+    };
+
+
+    template<int dim>
+    class AnalyticalVelocity : public TensorFunction<1, dim> {
+    public:
+        Tensor<1, dim>
+        value(const Point<dim> &p) const override;
+
+        Tensor<2, dim>
+        gradient(const Point<dim> &p) const override;
+    };
+
+
+    template<int dim>
+    class AnalyticalPressure : public Function<dim> {
+    public:
+        double
+        value(const Point<dim> &p, const unsigned int component) const override;
+
+        Tensor<1, dim>
+        gradient(const Point<dim> &p,
+                 const unsigned int component) const override;
+    };
+
+}
+
+
+#endif // MICROBUBBLE_RHS_GEN_H
