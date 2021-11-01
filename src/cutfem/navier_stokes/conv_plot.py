cb92147d5be99295f96af284ff1ba6476cb24e61
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2021-11-01 11:44:31 +0100

    Convergence plots for Navier-Stokes.

---
+++ src/cutfem/navier_stokes/conv_plot.py
@@ -0,0 +1,62 @@
+import os
+from os.path import split
+import numpy as np
+import matplotlib.pyplot as plt
+
+from utils.plot import conv_plots, eoc_plot, time_error_plots, conv_plots2
+
+
+def convergence_plot_report():
+    # A report ready convergence plot
+    paths = [os.path.join(base, f"build/src/cutfem/navier_stokes", folder, f"errors-d2o{d}.csv") for d in element_order]
+    plot_for = [r"\|u\|_{L^2L^2}", r"\|u\|_{L^2H^1}", "\|p\|_{L^2L^2}", "\|p\|_{L^2H^1}", "\|u\|_{l^\infty L^2}",
+                "\|u\|_{l^\infty H^1}"]
+    conv_plots2(paths, plot_for, element_order, expected_degrees=[[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]],
+                domain_length=domain_length, save_figs=True, font_size=12, label_size="large", skip=1,
+                guess_degree=False, flip_triangle=(True, False), tight=True)
+
+
+def time_error_plot():
+    for d in element_order:
+        paths = [os.path.join(base, f"build/src/cutfem/navier_stokes", folder, f"errors-time-d2o{d}r{r}.csv") for r in
+                 range(3, 8)]
+        time_error_plots(paths, data_indices=[3, 4, 5, 6, 7, 8], font_size=12, label_size="large",
+                         title=f"Navier-Stokes Equations time error, element order ({d + 1}, {d})", save_fig=True,
+                         identifier=d)
+
+
+# Plot settings
+folder = ""
+radius = 0.05
+end_time = radius
+domain_length = radius
+xlabel = "M"
+element_order = [1, 2]
+
+if __name__ == '__main__':
+    base = split(split(split(os.getcwd())[0])[0])[0]
+
+    time_error_plot()
+    # convergence_plot_report()
+
+    skip = 0
+    for poly_order in element_order:
+        full_path = os.path.join(base, "build/src/cutfem/navier_stokes", folder, f"errors-d2o{poly_order}.csv")
+
+        head = list(map(str.strip, open(full_path).readline().split(",")))[1:]
+        data = np.genfromtxt(full_path, delimiter=",", skip_header=True)
+        # data[:, 1] = data[:, 0]  # TODO only for stationary problems
+        data = data[skip:, 1:]
+
+        conv_plots(data, head, title=r"$\textrm{Navier-Stokes, element order: (" + str(
+            poly_order + 1) + ", " + str(poly_order) + ")}$", domain_length=domain_length, xlabel=xlabel)
+        plt.savefig(f"bdf2-error-o{poly_order}.pdf")
+
+        # Create a EOC-plot
+        eoc_plot(data, head,
+                 title=r"\textrm{Navier-Stokes EOC, element order: (" + str(poly_order + 1) + ", " + str(
+                     poly_order) + ")}",
+                 domain_lenght=domain_length, lines_at=np.array([1, 2]), xlabel=xlabel)
+        plt.savefig(f"bdf2-eoc-o{poly_order}.pdf")
+
+    plt.show()
---
+++ src/cutfem/navier_stokes/tau_comparison.py
@@ -0,0 +1,30 @@
+import os
+from os.path import split
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+from utils.plot import eoc_plot_after_cut_off_time
+
+# Use colours from the Plasma colour map.
+cmap = matplotlib.cm.get_cmap("plasma")
+
+base = split(split(split(os.getcwd())[0])[0])[0]
+
+
+if __name__ == '__main__':
+    build_base = os.path.join(base, "build/src/cutfem/navier_stokes")
+    factors = [1]
+    folder_names = [""]
+
+    radius = 0.15
+    end_time = radius
+    cutoff_time = end_time / 4
+    n_refines = range(3, 7)
+
+    data_columns = [3, 4, 5, 6, 7, 8]
+    max_norm_indices = [3, 4]
+    max_norm_names = [r'\|u\|_{l^\infty L^2}', r'\|u\|_{l^\infty H^1}']
+    eoc_plot_after_cut_off_time(build_base, factors, folder_names, end_time, cutoff_time, n_refines,
+                                data_columns, max_norm_indices, max_norm_names)
+    plt.show()
